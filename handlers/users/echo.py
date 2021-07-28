d4c329a65ac992231f6f86307909023bdba3474b
Anvar Narzullaev <anvarbek@gmail.com>
2021-07-28 20:05:43 +0800

    Shablon yangilandi

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,21 +1,9 @@
 from aiogram import types
-from aiogram.dispatcher import FSMContext
 
 from loader import dp
 
 
-# state ko'rsatilmagan barcha xabarlar uchun handler
+# Echo bot
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
-    await message.answer(f"state ko'rsatilmadi."
-                         f"Xabar:\n"
-                         f"{message.text}")
-
-
-# state="*" xabarlar uchun handler
-@dp.message_handler(state="*", content_types=types.ContentTypes.ANY)
-async def bot_echo_all(message: types.Message, state: FSMContext):
-    state = await state.get_state()
-    await message.answer(f"<code>state={state}</code>.\n"
-                         f"\nXabar:\n"
-                         f"<code>{message}</code>")
+    await message.answer(message.text)
